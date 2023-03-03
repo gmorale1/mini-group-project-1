fb01ea1394f42123b329c8f1d7ce22495dde12df
gadmorales <gmorales@dbsgroup.net>
2023-03-03 16:26:31 -0500

    printscreen update

--- src/SpaceGame/PrintScreen.ino
+++ src/SpaceGame/PrintScreen.ino
@@ -2,7 +2,16 @@ int columnList[8] = {0, 1, 2, 3, 4, 5, 6, 7};
 int redRowList[8] = {22, 24, 26, 28, 30, 32, 34, 36};
 int grnRowList[8] = {23, 25, 27, 29, 31, 33, 35, 37};
 
-int printable[8][8];
+int printable[8][8] = {
+    {1,1,1,1,1,1,1,1},
+    {1,1,1,1,1,1,1,1},
+    {1,1,0,1,1,0,1,1},
+    {1,1,0,1,1,0,1,1},
+    {1,1,1,1,1,1,1,1},
+    {1,0,1,1,1,1,0,1},
+    {1,1,0,0,0,0,1,1},
+    {1,1,1,1,1,1,1,1}
+    };
 
 void setup() {
   // put your setup code here, to run once:
@@ -10,13 +19,16 @@ void setup() {
     pinMode(columnList[i], OUTPUT);
     pinMode(redRowList[i], OUTPUT);
     pinMode(grnRowList[i], OUTPUT);
+//    digitalWrite(redRowList[i], HIGH);
+//    digitalWrite(grnRowList[i], LOW);
+//    digitalWrite(columnList[i], HIGH);
   }
   
-  for (int i = 0; i < 8; i++) {
-    for (int j = 0; j < 8; j++) {
-      printable[i][j] = 1;
-    }
-  }
+//  for (int i = 0; i < 8; i++) {
+//    for (int j = 0; j < 8; j++) {
+//      printable[i][j] = 2;
+//    }
+//  }
 }
 
 void loop() {
@@ -26,29 +38,35 @@ void loop() {
 
 void PrintToScreen (int printMatrix[8][8]) {
     for (int i = 0; i < 8; i++) {
+      //            digitalWrite(columnList[j], LOW);
         for (int j = 0; j < 8; j++) {
 
             //NOTE going to have to experiment with what happens when the row values are LOW and the COLUMN values are high
             //Determines which row will be lighting up
             if (printMatrix[i][j] == 1) {
+                digitalWrite(grnRowList[i], HIGH);
                 digitalWrite(redRowList[i], LOW);
+                digitalWrite(columnList[j], HIGH);
             }
             else if (printMatrix[i][j] == 2) {
+                digitalWrite(redRowList[i], HIGH);
                 digitalWrite(grnRowList[i], LOW);
+                digitalWrite(columnList[j], HIGH);
             }
             else {
                 digitalWrite(redRowList[i], HIGH);
                 digitalWrite(grnRowList[i], HIGH);
+                digitalWrite(columnList[j], LOW);                
             }
-
             //Wherever a HIGH row and a LOW column meet is where the light will light up
-            digitalWrite(columnList[j], HIGH);
+//            digitalWrite(columnList[j], LOW);
 
-            //Sets back to values where nothing should light up when a row value is changed;
-            digitalWrite(redRowList[i], HIGH);
-            digitalWrite(grnRowList[i], HIGH);
-            digitalWrite(columnList[j], HIGH);
-
+          
         }
+        delay(100/30);
+        //Sets back to values where nothing should light up when a row value is changed;
+        digitalWrite(redRowList[i], HIGH);
+        digitalWrite(grnRowList[i], HIGH);
+//        digitalWrite(columnList[i], LOW);
     }
 }
--- src/SpaceGame/SpaceGame.ino
+++ src/SpaceGame/SpaceGame.ino
@@ -1,13 +1,1 @@
-void setup() {
-  // put your setup code here, to run once:
 
-}
-
-void loop() {
-  // put your main code here, to run repeatedly:
-
-}
-
-void activateLED(int x, int y, int red, int green){
-  
-}
