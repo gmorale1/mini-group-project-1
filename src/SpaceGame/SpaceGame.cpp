144d6a7cb64bae645592b5a8c78ed84720f539a8
jacobgpage <63179397+jacobgpage@users.noreply.github.com>
2023-03-08 14:36:45 -0500

    Should be the final includes collision detection!

--- src/SpaceGame/SpaceGame.cpp
+++ src/SpaceGame/SpaceGame.cpp
@@ -1,3 +1,12 @@
+/*
+TODO
+- Implement player movement
+- Implement way to keep track of score
+
+OPTIONAL
+- Implement collision detectin between asteroids
+*/
+
 #include <iostream>
 #include <stdlib.h>
 #include<unistd.h>
@@ -6,15 +15,6 @@
 
 using namespace std;
 
-/*
-TODO
-- Implement player movement
-- Implement collision detection between player and asteroids
-- Implement way to keep track of score
-
-OPTIONAL
-- Implement collision detectin between asteroids
-*/
 
 //Object used to manage the asteroids position and direction
 class Asteroid {
@@ -40,6 +40,7 @@ class Player {
     public:
         int x[5];
         int y[5];
+        int lives;
 
     Player () {
         x[0] = 3;
@@ -52,6 +53,7 @@ class Player {
         y[2] = 5;
         y[3] = 4;
         y[4] = 4;
+        lives = 3;
     }
 };
 
@@ -74,6 +76,7 @@ void PrintMatrix (int matrix[8][8]) {
 int main () {
     int matrix[8][8];
     int maxAsteroids = 2;
+    int isGameAlive = true;
 
     Asteroid objectList[maxAsteroids];
     Player playerOne;
@@ -95,10 +98,11 @@ int main () {
 
     int spawnTimer = time(NULL);
 
-    while(true) {
+    while(isGameAlive) {
         PrintMatrix(matrix);
 
         //Spawns asteroid every 5 seconds if there is an asteroid to spawn
+        //Change max asteroids to enable for asteroids to spawn at one time
         if ((time(NULL) - spawnTimer) > 5) {
             for (int i = 0; i < maxAsteroids; i++) {
                 if (objectList[i].isActive == false) {
@@ -118,48 +122,91 @@ int main () {
 
             if (objectList[i].isActive == true) {
                 if ((time(NULL) - objectList[i].startTime) < 10 && objectList[i].isBlinking == false) {
-                matrix[objectList[i].x][objectList[i].y] = 5;
-                matrix[objectList[i].x+1][objectList[i].y+1] = 5;
-                matrix[objectList[i].x][objectList[i].y+1] = 5;
-                matrix[objectList[i].x-1][objectList[i].y+1] = 5;
-                matrix[objectList[i].x-1][objectList[i].y] = 5;
-                matrix[objectList[i].x-1][objectList[i].y-1] = 5;
-                matrix[objectList[i].x][objectList[i].y-1] = 5;
-                matrix[objectList[i].x+1][objectList[i].y-1] = 5;
-                matrix[objectList[i].x+1][objectList[i].y] = 5;
-                if ((time(NULL) - objectList[i].startTime) < 5) {
-                    objectList[i].isBlinking = true;
+                    //Tells print function to print the asteroids as 0
+                    //IMPORTANT Just set to either 1 or 2 to work with our print screen function
+                    matrix[objectList[i].x][objectList[i].y] = 5;
+                    matrix[objectList[i].x+1][objectList[i].y+1] = 5;
+                    matrix[objectList[i].x][objectList[i].y+1] = 5;
+                    matrix[objectList[i].x-1][objectList[i].y+1] = 5;
+                    matrix[objectList[i].x-1][objectList[i].y] = 5;
+                    matrix[objectList[i].x-1][objectList[i].y-1] = 5;
+                    matrix[objectList[i].x][objectList[i].y-1] = 5;
+                    matrix[objectList[i].x+1][objectList[i].y-1] = 5;
+                    matrix[objectList[i].x+1][objectList[i].y] = 5;
+                    if ((time(NULL) - objectList[i].startTime) < 5) {
+                        objectList[i].isBlinking = true;
+                    }
                 }
-            }
-            else {
-                matrix[objectList[i].x][objectList[i].y] = 0;
-                matrix[objectList[i].x+1][objectList[i].y+1] = 0;
-                matrix[objectList[i].x][objectList[i].y+1] = 0;
-                matrix[objectList[i].x-1][objectList[i].y+1] = 0;
-                matrix[objectList[i].x-1][objectList[i].y] = 0;
-                matrix[objectList[i].x-1][objectList[i].y-1] = 0;
-                matrix[objectList[i].x][objectList[i].y-1] = 0;
-                matrix[objectList[i].x+1][objectList[i].y-1] = 0;
-                matrix[objectList[i].x+1][objectList[i].y] = 0;
-                objectList[i].isBlinking = false;
+                else {
+                    matrix[objectList[i].x][objectList[i].y] = 0;
+                    matrix[objectList[i].x+1][objectList[i].y+1] = 0;
+                    matrix[objectList[i].x][objectList[i].y+1] = 0;
+                    matrix[objectList[i].x-1][objectList[i].y+1] = 0;
+                    matrix[objectList[i].x-1][objectList[i].y] = 0;
+                    matrix[objectList[i].x-1][objectList[i].y-1] = 0;
+                    matrix[objectList[i].x][objectList[i].y-1] = 0;
+                    matrix[objectList[i].x+1][objectList[i].y-1] = 0;
+                    matrix[objectList[i].x+1][objectList[i].y] = 0;
+                    objectList[i].isBlinking = false;
 
-                if ((time(NULL) - objectList[i].startTime) > 10) {
-                    objectList[i].isActive = false;
+                    if ((time(NULL) - objectList[i].startTime) > 10) {
+                        objectList[i].isActive = false;
+                    }
                 }
-            }
             }
         }
 
         //Implement player input here
 
-        //Implement check for player asteroid collision
         for (int i = 0; i < 5; i++) {
+            //Player is colliding with an asteroid
+            if (matrix[playerOne.x[i]][playerOne.y[i]] == 5) {
+
+                //Destroy asteroid that collided with player
+                for (int j = 0; j < maxAsteroids; j++) {
+                    for (int k = 0; k < 5; k++) {
+                        if ((objectList[j].x == playerOne.x[k] || objectList[j].x+1 == playerOne.x[k] || objectList[j].x-1 == playerOne.x[k]) && (objectList[j].y == playerOne.y[k] || objectList[j].y+1 == playerOne.y[k] || objectList[j].y-1 == playerOne.y[k])) {
+                            //Found which asteroid the player has collided with or at least an asteroid
+                            if ((time(NULL) - objectList[j].startTime) > 5 && (time(NULL) - objectList[j].startTime) < 10) {
+                                //The asteroid found can damage the player
+                                playerOne.lives--;
+                                objectList[j].isActive = false;
+
+                                //Make sure all reminents of the destroyed asteroid dont show
+                                matrix[objectList[j].x][objectList[j].y] = 0;
+                                matrix[objectList[j].x+1][objectList[j].y+1] = 0;
+                                matrix[objectList[j].x][objectList[j].y+1] = 0;
+                                matrix[objectList[j].x-1][objectList[j].y+1] = 0;
+                                matrix[objectList[j].x-1][objectList[j].y] = 0;
+                                matrix[objectList[j].x-1][objectList[j].y-1] = 0;
+                                matrix[objectList[j].x][objectList[j].y-1] = 0;
+                                matrix[objectList[j].x+1][objectList[j].y-1] = 0;
+                                matrix[objectList[j].x+1][objectList[j].y] = 0;
+
+                                j = 100;
+                                break;
+                            }
+                        }
+                    }
+                }
+
+                //Check for gameover
+                if (playerOne.lives <= 0) {
+                    isGameAlive = false;
+                }
+            }
+            //Tells print function to print the player as +
+            //IMPORTANT Just set to either 1 or 2 to work with our print screen function
             matrix[playerOne.x[i]][playerOne.y[i]] = 6;
         }
 
+        cout << playerOne.lives << endl;
+        //IMPORTANT might have to adjust the following to keep print from flickering and such
         sleep(1);
+        //IMPORTANT remove the following live before trying to use with arduino
         system("clear");
     }
 
+    cout << "Final Score: " + (time(NULL) - gameStart) + endl << "GAME OVER" << endl;
     return(0);
 }
